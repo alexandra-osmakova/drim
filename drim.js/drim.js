8754cb7d6f835153075d537a7117f7c8b3fc7531
Александра Осмакова <aleksandra.osmakova@MacBook-Pro-Aleksandra.local>
2018-09-17 14:48:25 +0300

    circle section animated, oue_works and services improved

---
+++ drim.js/drim.js
@@ -0,0 +1,22 @@
+var wrap = document.getElementsByClassName('btns')[0];
+var text = document.getElementsByClassName('circle_text_item');
+
+window.onload = function() {
+	text[0].classList.add('circle_text_visible')
+}
+
+wrap.addEventListener('mouseover', go, false);
+
+function go(e) {
+	if (e.target.classList.contains('circle_content_item')) {
+        var target = e.target.dataset.number;
+    for(var i = 0; i < text.length; i++) {
+    	if(text[i].dataset.number == target) {
+      	text[i].classList.add('circle_text_visible');
+      } 
+      else {
+      	text[i].classList.remove('circle_text_visible');
+      }
+    }
+  }
+};
