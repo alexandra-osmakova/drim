2e3ea033eb5421ec644cac832dfdaf3eb53a1713
Александра Осмакова <aleksandra.osmakova@MacBook-Pro-Aleksandra.local>
2018-09-18 12:18:06 +0300

    comments are added

--- drim.js/drim.js
+++ drim.js/drim.js
@@ -22,3 +22,52 @@ function go_visible(e) {
 };
 
   AOS.init();
+
+
+  var isScrolling = false;
+  var pagination = document.getElementsByClassName('pagination_circle');
+
+window.addEventListener("scroll", throttleScroll, false);
+
+function throttleScroll(e) {
+    if (isScrolling == false) {
+        window.requestAnimationFrame(function () {
+            var inProgress = document.getElementsByClassName('screen');
+            for (var i = 0; i < inProgress.length; i++) {
+                scrolling(inProgress[i]);
+                isScrolling = false;
+            }
+        });
+    }
+    isScrolling = true;
+}
+
+function scrolling(el) {
+    if (isFullyVisible(el)) {
+    		for(var i = 0; i < pagination.length; i++) {
+       		pagination[i].classList.add("pagination_circle_activ");
+        }
+    } else {
+    		pagination[i].classList.remove("pagination_circle_activ");
+    }
+}
+
+
+function isPartiallyVisible(el) {
+    var elementBoundary = el.getBoundingClientRect();
+
+    var top = elementBoundary.top;
+    var bottom = elementBoundary.bottom;
+    var height = elementBoundary.height;
+
+    return ((top + height >= 0) && (height + window.innerHeight >= bottom));
+}
+
+function isFullyVisible(el) {
+    var elementBoundary = el.getBoundingClientRect();
+
+    var top = elementBoundary.top;
+    var bottom = elementBoundary.bottom;
+
+    return ((top >= 0) && (bottom <= window.innerHeight));
+}
