a1d99b28def965f52f676d6f1b4d130e9f9af2b2
Александра Осмакова <aleksandra.osmakova@MacBook-Pro-Aleksandra.local>
2018-09-20 13:47:54 +0300

    code for sending file is added

--- mail.php
+++ mail.php
@@ -8,47 +8,36 @@
     $development = $_POST['development'];
     $support = $_POST['support'];
     $advertising = $_POST['advertising'];
+    $boundary = md5(rend());
 
+    $headers = array(
+        'MIME Version: 1.0'.
+        'From: {$email_from}'.
+        'Content-Type: multipart/mixed; boundary = {$boundary}'
+        );
 
-    
-    /*$message ="Текст сообщения";
-    
-    $filename = "file.doc";
-    $filepath = "files/file.doc";
-    
-    
-    $boundary = "--".md5(uniqid(time())); 
-    
-    $mailheaders = "MIME-Version: 1.0;\r\n"; 
-    $mailheaders .="Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n"; 
-    
-    $multipart = "--$boundary\r\n"; 
-    $multipart .= "Content-Type: text/html; charset=windows-1251\r\n";
-    $multipart .= "Content-Transfer-Encoding: base64\r\n";    
-    $message_part = '\r\n';
-    $multipart .= chunk_split(base64_encode(iconv("utf8", "windows-1251", $message)));
-     
-        $fp = fopen($filepath,"r"); 
-            if (!$fp) 
-            { 
-                print "Не удается открыть файл22"; 
-                exit(); 
-            } 
-    $file = fread($fp, filesize($filepath)); 
-    fclose($fp); 
-    
-    
-    $message_part = "\r\n--$boundary\r\n"; 
-    $message_part .= "Content-Type: application/octet-stream; name=\"$filename\"\r\n";  
-    $message_part .= "Content-Transfer-Encoding: base64\r\n"; 
-    $message_part .= "Content-Disposition: attachment; filename=\"$filename\"\r\n"; 
-    $message_part .= \r\n;
-    $message_part .= chunk_split(base64_encode($file));
-    $message_part .= "\r\n--$boundary--\r\n";
-    
-    $multipart .= $message_part; */
+    $message = array(
+        "--{$boundary}".
+        "Content-Type: text/plain: charset = utf-8 ".
+        "Content-Transform-Encoding: 7bit".
+        "".
+        chunk_split($body).
+        "--{$boundary}".
+        "Content-Type: ($file['type']); name = ($file ['name'])".
+        "Content-Disposition: attachment; filename = ($file [name]).
+        "Content-Transfer-Encoding: base64".
+        "".
+        chunk_split(base64_encode(file_get_contacts($file['path']))).
+        "--{$boundary}--".
+    )
 
-    
+    if(isset($_FILE['uploaded_file'])) {
+        $file = array();
+        'name' => $_FILE['file']['name'],
+        'size' => $_FILE['file']['size'],
+        'type' => $_FILE['file']['type'],
+        'path' => $_FILE['file']['tmp_name'],
+    }
 
     $email_from = 'alexandra.osmakova@gmail.com';
 
@@ -61,7 +50,14 @@
                     "User Message: $apply_task.\n".
                     "Development: $development.\n".
                     "Support: $support.\n".
-                    "Advertising: $advertising.\n";
+                    "Advertising: $advertising.\n".
+                    "File details:
+                            Name: {$file['name']}.
+                            Size: {$file['size']}.
+                            Type: {$file['type']}";
+
+
+
 
 
             
@@ -70,9 +66,5 @@
     $to = "alexandra.osmakova@gmail.com";
     $headers = "From: $email_from \r\n";
 
-    $ok = mail($to, $email_subject, $email_body, $headers);
-
-    /*if (time_nanosleep(5, 0)) {
-        unlink($filepath);
-} */
+    $ok = mail($to, $email_subject, $email_body, implode('\r\n', $message). implode('\r\n', $headers) $file, $headers);
 ?>
