92241972eb6892341a0364251e5d9070e2a3f067
Александра Осмакова <aleksandra.osmakova@MacBook-Pro-Aleksandra.local>
2018-10-15 14:06:10 +0300

    slider added. everything is fixed. animation and adaptation have to be added

--- drim_inner_page__create/drim_inner_page_create__js/drim.js
+++ drim_inner_page__create/drim_inner_page_create__js/drim.js
@@ -15,14 +15,43 @@ menu__icon.addEventListener('click', function () {
 
 document.body.addEventListener('touchstart', function () {});
 
-var slider = tns({
-    container: '.dev_steps_slider',
-    loop: false,
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    swipeAngle: false,
-    speed: 400,
-    autoWidth: true,
-    controlsText:"  ",
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
+
+
+  var swiperSlide = document.getElementsByClassName('swiper-slide ');
+  var devStepsImagesItem = document.getElementsByClassName('dev_steps__images_item');
+
+  var swiperButtonNext = document.getElementsByClassName('swiper-button-next')[0];
+  var swiperButtonPrev = document.getElementsByClassName('swiper-button-prev')[0];
+
+  swiperButtonNext.addEventListener('click', swiperItemGoesActiv);
+  swiperButtonPrev.addEventListener('click', swiperItemGoesActivBack);
+
+  window.onload = function() {
+    devStepsImagesItem[0].classList.add('activ_item');
+  }
+
+   function swiperItemGoesActiv() {
+    for(var i = 0; i < swiperSlide.length; i++) {
+        if(swiperSlide[i].classList.contains('swiper-slide-active')) {
+            devStepsImagesItem[i].classList.add('activ_item');
+            devStepsImagesItem[i-1].classList.remove('activ_item')
+        }
+    }
+  }
+
+  function swiperItemGoesActivBack() {
+    for(var i = 0; i < swiperSlide.length; i++) {
+        if(swiperSlide[i].classList.contains('swiper-slide-active')) {
+            devStepsImagesItem[i].classList.add('activ_item');
+            devStepsImagesItem[i+1].classList.remove('activ_item')
+        }
+    }
+  }
+
+  AOS.init();
